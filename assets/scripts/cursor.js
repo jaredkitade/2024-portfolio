68c8658622791215ff4f4cd90e61b2b4287cb891
jaredkitade <jkitade@gmail.com>
2024-06-15 11:41:55 -0700

    fonts

---
+++ assets/scripts/cursor.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+
+    var cursor = $(".cursor-default");
+
+    $(window).mousemove(function(e) {
+        cursor.css({
+            top: e.clientY - cursor.height() / 2,
+            left: e.clientX - cursor.width() / 2
+        });
+    });
+
+    $(window)
+        .mouseleave(function() {
+            cursor.css({
+                opacity: "0"
+            });
+        })
+        .mouseenter(function() {
+            cursor.css({
+                opacity: "1"
+            });
+        });
+
+    // $(".link")
+    //     .mouseenter(function() {
+    //         cursor.css({
+    //             transform: "scale(3.2)"
+    //         });
+    //     })
+    //     .mouseleave(function() {
+    //         cursor.css({
+    //             transform: "scale(1)"
+    //         });
+    //     });
+
+    $(".featured-project-tile")
+        .mouseenter(function() {
+            cursor.addClass('cursor-learn-more');
+            // cursor.removeClass('cursor-default');
+        })
+        .mouseleave(function() {
+            cursor.removeClass('cursor-learn-more');
+            // cursor.addClass('cursor-default');
+        });    
+
+    $(window)
+        .mousedown(function() {
+            cursor.css({
+                transform: "scale(4)"
+            });
+        })
+        .mouseup(function() {
+            cursor.css({
+                transform: "scale(1)"
+            });
+        });
+       
+        
+    });
