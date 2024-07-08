d965c36845548aeda2e94897ec3ac3012f458d0c
jaredkitade <jkitade@gmail.com>
2024-07-08 16:57:00 -0700

    Update nav-bar.js
    home page load state fix

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -19,11 +19,16 @@ $(document).ready(function () {
         var scroll = $(window).scrollTop();
         var offset = $('.home-page-hero').offset().top;
         var height = $('.home-page-hero').height();
+        
         if(scroll > offset + height - 75){
             $('.nav').removeClass('negative');
-            $('body').removeClass('home-page-load-bg');
         } else {
         	$('.nav').addClass('negative');
+        }
+
+        if(scroll > offset + height){
+            $('body').removeClass('home-page-load-bg');
+        } else {
         	$('body').addClass('home-page-load-bg');
         }
 
@@ -31,7 +36,7 @@ $(document).ready(function () {
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        if(scroll > 50) {
+        if(scroll > 40) {
             $('.mobile-nav').removeClass('negative');
         } else {
         	$('.mobile-nav').addClass('negative');
