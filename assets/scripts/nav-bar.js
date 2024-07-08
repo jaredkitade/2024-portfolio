b1753c4177c37c488db685338d8443ec4af3f3c2
jaredkitade <jkitade@gmail.com>
2024-07-08 14:39:11 -0700

    mobile nav fix

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 	$(".menu").click(function(){
 		$(".nav").toggleClass("nav-open");
 		$(".nav-container").toggleClass("nav-container-open");
+		$(".mobile-nav-container").toggleClass("nav-container-open");
 		$(".nav-item").toggleClass("nav-item-open");
 		$(".menu").toggleClass("close-menu");
 		$(".work").toggleClass("nav-item-off");
