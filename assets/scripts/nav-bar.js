8ea4f2fa6b342f90902616b17f8f247d57852507
jaredkitade <jkitade@gmail.com>
2024-07-08 12:39:29 -0700

    mobile - nav/hover

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
+
 	$(".menu").click(function(){
 		$(".nav").toggleClass("nav-open");
 		$(".nav-container").toggleClass("nav-container-open");
+		$(".nav-item").toggleClass("nav-item-open");
 		$(".menu").toggleClass("close-menu");
 		$(".work").toggleClass("nav-item-off");
 		$(".about").toggleClass("nav-item-off");
 		$("body").toggleClass("no-scroll");
 		$("body").toggleClass("body-menu-open");
-		// $("body").toggleClass("gradient-background-warm");
 		$(".page-container").toggleClass("page-container-menu-open");
 		$(".home-page-container").toggleClass("page-container-menu-open");
 	});
